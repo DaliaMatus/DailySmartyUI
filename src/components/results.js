0433c8fb0f65379429e1c6aaae6939dedd109b8e
DaliaMatus <daliamatus@gmail.com>
2020-06-10 02:23:20 -0500

    styled results component

--- src/components/results.js
+++ src/components/results.js
@@ -14,7 +14,7 @@ class Results extends Component{
 
     render(){
         return(
-            <div>
+            <div className="results">
                 <Logo size ={55}/>
                 <SearchBar  onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
                 <ResultsPosts/>
