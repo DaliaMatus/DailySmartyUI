8d1d7c09f85dd13b4ed2e9808995601dead9ebc8
DaliaMatus <daliamatus@gmail.com>
2020-06-09 19:13:05 -0500

    implemented withRouter HOC into the SearchBar to navigate the user to results route

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
 import {Field, reduxForm} from "redux-form";
+import{withRouter} from "react-router-dom";
 
 class SearchBar extends Component{
     handleFormSubmit=function({query}) {
         console.log('trying to handle submit for query', query);
+        this.props.history.push("/results");
     }
 
     renderInput(field) {
@@ -23,5 +25,7 @@ class SearchBar extends Component{
 SearchBar = reduxForm({
     form: 'searchBar'
 })(SearchBar);
+
+SearchBar = withRouter(SearchBar);
 
 export default SearchBar;
